999f65965b738cc33b4149715b0ef78732d7d3e8
Bohdan Bokariev <b.bokariev@gmail.com>
2023-07-07 14:53:48 +0300

    Updated base code for assistant.py(PEP 8 Rules included)

--- assistant.py
+++ assistant.py
@@ -1,273 +1,190 @@
-import sys 
-from collections import UserDict
-from datetime import datetime
 import re
 import pickle
-
-STOP_WORDS = ['exit', 'good bye', 'close']
+from datetime import date, datetime
+from collections import UserDict
 
 
 class Field:
-    def __init__(self, value=None):
+    def __init__(self, value):
+        self._value = None
         self.value = value
-    
+
     @property
     def value(self):
         return self._value
 
     @value.setter
     def value(self, new_value):
+        self.validate(new_value)
         self._value = new_value
 
-    def __repr__(self):
-        return f'{self.value}'
-    
+    def validate(self, value):
+        pass
+
 
 class Name(Field):
     pass
-            
+
 
 class Phone(Field):
-    @Field.value.setter
-    def value(self, new_value):
-        if not self._is_valid_phone(new_value):
-            raise ValueError("Invalid phone number")
-        self._value = new_value
+    PHONE_REGEX = re.compile(r"^\+?(\d{2})?\(?\d{3}\)?[\d\-\s]{7,10}$")
 
-    @staticmethod
-    def _is_valid_phone(phone):
-        phone_regex = r'^\+?3?8?(0\d{9})$'
-        if re.search(phone_regex, phone):
-            return True
-        else:
-            raise ValueError('Please write phone number in proper format')
+    def validate(self, phone):
+        if not self.PHONE_REGEX.match(phone):
+            raise ValueError(f"Phone number {phone} is invalid.")
 
 
 class Birthday(Field):
-    @Field.value.setter
+    DATE_REGEX = re.compile(r"^\d{4}-\d{2}-\d{2}$")
+
+    def validate(self, date_str):
+        if not self.DATE_REGEX.match(date_str):
+            raise ValueError(f"Invalid date format: {date_str}. Expected format: YYYY-MM-DD.")
+
+    @property
+    def value(self):
+        if self._value:
+            return datetime.strptime(self._value, "%Y-%m-%d").date()
+        return None
+
+    @value.setter
     def value(self, new_value):
-        if not self._is_valid_birthday(new_value):
-            raise ValueError("Invalid birthday")
+        self.validate(new_value)
         self._value = new_value
 
-    @staticmethod
-    def _is_valid_birthday(birthday):
-        birthday_regex = r'\d{2}-\d{2}-\d{4}'
-        if re.search(birthday_regex, birthday):
-            return True
-        else: 
-            raise ValueError('Please write date of birth in format: dd-mm-yyyy')
-    
 
 class Record:
-    def __init__(self, name, phone=None, birthday=None):
-        self.name = Name(name)
-        if phone:
-            self.phones = []
-            self.phones.append(Phone(phone))
-        if birthday:
-            self.birthday = Birthday(birthday)
-        
-    def add_phone(self, phone):
-        self.phones.append(Phone(phone))
-        
-    def remove_phone(self, phone):
-        self.phones = [p for p in self.phones if p.value != phone]
-        
-    def edit_phone(self, old_phone, new_phone):
-        for phone in self.phones:
-            if phone.value == old_phone:
-                phone.value = new_phone
-                break
-            
+    def __init__(self, name, birthday=None):
+        self.name = name
+        self.phones = []
+        self.birthday = birthday
+
+    def add(self, phone):
+        self.phones.append(phone)
+
     def days_to_birthday(self):
-        if not hasattr(self, 'birthday') or not self.birthday.value:
-            return None
+        if self.birthday and self.birthday.value:
+            today = date.today()
+            next_birthday = date(today.year, self.birthday.value.month, self.birthday.value.day)
+            if today > next_birthday:
+                next_birthday = date(today.year + 1, self.birthday.value.month, self.birthday.value.day)
+            days_left = (next_birthday - today).days
+            return days_left
+        return None
 
-        today = datetime.now().date()
-        birthday = datetime.strptime(self.birthday.value, "%d-%m-%Y").date().replace(year=today.year)
-
-        if birthday < today:
-            birthday = birthday.replace(year=today.year + 1)
-
-        return (birthday - today).days
-    
-    def __repr__(self):
-        if not hasattr(self, 'birthday') or not self.birthday.value:
-            return f'Phones:{self.phones}'
-        else:
-            return f'Phones:{self.phones}, Birthday:{self.birthday}'
-        
 
 class AddressBook(UserDict):
     def add_record(self, record):
         self.data[record.name.value] = record
-        
-    def iterator(self, page_size):
-        records = list(self.data.values())
-        total_pages = len(records) // page_size + 1
 
-        for page in range(total_pages):
-            start = page * page_size
-            end = start + page_size
-            yield records[start:end]
-    
-    def save_data(self, file_path):
-        with open(file_path, 'wb') as file:
-            pickle.dump(self.data, file)
+    def remove_record(self, name):
+        self.data.pop(name, None)
 
-    def load_data(self, file_path):
-        with open(file_path, 'rb') as file:
-            self.data = pickle.load(file)
+    def get_all_records(self):
+        return self.data.values()
 
-    def search_contacts(self, search_string):
-        search_string = search_string.lower()
-        matching_contacts = []
-
+    def search(self, query):
+        result = []
         for record in self.data.values():
-            if search_string in record.name.value.lower():
-                matching_contacts.append(record)
-            else:
-                for phone in record.phones:
-                    if search_string in phone.value.lower():
-                        matching_contacts.append(record)
-                        break
-
-        return matching_contacts
-            
-
-contacts = AddressBook()
+            if query.lower() in record.name.value.lower() or any(query in phone.value for phone in record.phones):
+                result.append(record)
+        return result
 
 
 def input_error(func):
-    def inner(*args, **kwargs):
+    def inner(*args):
         try:
-            result = func(*args, **kwargs)
-            return result
+            return func(*args)
+        except (IndexError, ValueError):
+            return "Помилка введення. Спробуйте ще раз."
         except KeyError:
-            print("Enter user name")
-        except ValueError:
-            print("Give me name and phone please")
-        except IndexError:
-            print("Invalid input. Please try again.")    
+            return "Контакту з таким ім'ям не знайдено."
     return inner
 
 
-def greet():
-    result = 'How can I help you?'
-    return result
+class Assistant:
+    SAVE_FILE = "address_book.pickle"
+
+    def __init__(self):
+        self.address_book = AddressBook()
+
+    @input_error
+    def hello(self, command_args):
+        return "Привіт! Я можу допомогти Вам з наступними командами: " \
+               "add, change name, phone name, show_all, birthday name, exit."
+
+    @input_error
+    def add(self, command_args):
+        name = input("Введіть ім'я: ")
+        birthday = input("Введіть день народження (у форматі YYYY-MM-DD): ")
+        record = Record(Name(name), Birthday(birthday))
+        phone = input("Введіть номер телефону: ")
+        record.add(Phone(phone))
+        self.address_book.add_record(record)
+        return "Запис додано."
+
+    @input_error
+    def change(self, command_args):
+        name = command_args.strip()
+        record = self.address_book.data.get(name)
+        if not record:
+            return "Контакту з таким ім'ям не знайдено."
+        phone = input("Введіть новий номер телефону: ")
+        record.phones = [Phone(phone)]
+        return "Зміни збережено."
+
+    @input_error
+    def phone(self, command_args):
+        name = command_args.strip()
+        record = self.address_book.data.get(name)
+        if not record:
+            return "Контакту з таким ім'ям не знайдено."
+        return ', '.join([str(phone.value) for phone in record.phones])
+
+    @input_error
+    def show_all(self, command_args):
+        return "\n".join([str(record.name.value) + ": " + ', '.join([str(phone.value) for phone in record.phones]) for record in self.address_book.get_all_records()])
+
+    @input_error
+    def birthday(self, command_args):
+        name = command_args.strip()
+        record = self.address_book.data.get(name)
+        if not record:
+            return "Контакту з таким ім'ям не знайдено."
+        days = record.days_to_birthday()
+        if days:
+            return f"До дня народження залишилося {days} днів."
+        else:
+            return "Дата народження не вказана."
+
+    @input_error
+    def exit(self, command_args):
+        self.save_data()
+        return "До побачення!"
+
+    def load_data(self):
+        try:
+            with open(self.SAVE_FILE, "rb") as file:
+                self.address_book = pickle.load(file)
+        except FileNotFoundError:
+            pass
+
+    def save_data(self):
+        with open(self.SAVE_FILE, "wb") as file:
+            pickle.dump(self.address_book, file)
 
 
-def new_contact(user_input):
-    parts = user_input.split(" ")
-    if len(parts) == 2:
-        record = Record(parts[1])
-    if len(parts) == 3:
-        record = Record(parts[1], parts[2])
-    if len(parts) == 4:
-        record = Record(parts[1], parts[2], parts[3])
-    contacts.add_record(record)   
-    result = 'Contact was successfully created'
-    return result
-
-
-def add_phone(user_input):
-    parts = user_input.split(" ")
-    contacts[parts[1]].add_phone(parts[2])
-    result = 'Extra phone was successfully added'
-    return result
-
-
-def remove_contact(user_input):
-    parts = user_input.split(" ")
-    contacts[parts[1]].remove_phone(parts[2])
-    result = 'Phone was removed'
-    return result
-    
-
-def change_contact(user_input):
-    parts = user_input.split(" ")
-    contacts[parts[1]].edit_phone(parts[2], parts[3])
-    result = 'Contact was successfully updated'
-    return result 
-
-
-def show_contact(user_input):
-    parts = user_input.split(" ")
-    return contacts[parts[1]]
-
-
-def show_all():
-    return contacts
-
-
-def days_to_birthday(user_input):
-    parts = user_input.split(" ")
-    result = str(contacts[parts[1]].days_to_birthday()) + ' days till birthday'
-    return result
-
-
-def search_contacts(user_input):
-    parts = user_input.split(" ")
-    search_string = ' '.join(parts[1:])
-    matching_contacts = contacts.search_contacts(search_string)
-    result = ''
-
-    if matching_contacts:
-        result += 'Matching contacts:\n'
-        for contact in matching_contacts:
-            result += str(contact) + '\n'
-    else:
-        result = 'No matching contacts found'
-
-    return result
-        
-
-def process_input(user_input):
-    listed_user_input = user_input.split(' ')
-    command = listed_user_input[0].lower()
-    if command == 'hello':
-        return greet()
-    elif command == 'create':
-        return new_contact(user_input)
-    elif command == 'add':
-        return add_phone(user_input)
-    elif command == 'remove': 
-        return remove_contact(user_input)
-    elif command == 'change':
-        return change_contact(user_input)
-    elif command == 'phone': 
-        return show_contact(user_input)
-    elif command == 'show':
-        return show_all()
-    elif command == 'when':
-        return days_to_birthday(user_input)
-    elif command == 'search':
-        return search_contacts(user_input)
-        
-
-def run_bot():
+if __name__ == "__main__":
+    assistant = Assistant()
+    assistant.load_data()
+    print("Введіть команду:")
     while True:
-        user_input = input("Write your request: ")
-        if user_input not in STOP_WORDS:
-            print(process_input(user_input))
-        elif user_input in STOP_WORDS:
-            filename = input("Enter the filename to save the address book: ")
-            contacts.save_data(filename)
-            print('Good bye!')
-            return sys.exit()
-
-
-def start():
-    filename = input("Enter the filename to load the address book:")
-    if len(filename) > 0:
-        contacts.load_data(filename)
-        print("Address book loaded successfully!")
-        run_bot()
-    else:
-        run_bot()    
-        
-        
-if __name__ == '__main__':        
-    start()
-    
+        command = input("> ")
+        command_name, command_args = command.split(" ", 1) if " " in command else (command, "")
+        function = getattr(assistant, command_name, None)
+        if function:
+            print(function(command_args))
+        else:
+            print("Невідома команда. Спробуйте ще раз.")
+        if command_name == 'exit':
+            break
