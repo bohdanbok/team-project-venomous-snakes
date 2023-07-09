46a3c00be2bb5b22b7e84825633026371a7417a3
Bohdan Bokariev <b.bokariev@gmail.com>
2023-07-09 22:14:44 +0300

    Was done weather func

--- team_venomous_snakes/team_venomous_snakes/weather.py
+++ team_venomous_snakes/team_venomous_snakes/weather.py
@@ -7,28 +7,24 @@ API_KEY = '54bd96a092227d0950a024fdc4e6171e'
 def what_weather():
 
     city = input("Please enter city: ")
-    url = BASE_URL + "q=" + city + "uk&appid=" + API_KEY
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}'
     response = requests.get(url)
 
     if response.status_code == 200:
         data = response.json()
         main = data['main']
-        temperature = main['temp']
-        temp_feel_like = main['feels_like']
+        temperature = round(main['temp'] - 273.15)
+        temp_feel_like = round(main['feels_like'] - 273.15)
         humidity = main['humidity']
         pressure = main['pressure']
         weather_report = data['weather']
         wind_report = data['wind']
 
         print(f"{city:-^35}")
-        print(f"City ID: {data['id']}")
         print(f"Temperature: {temperature}")
         print(f"Feel Like: {temp_feel_like}")
-        print(f"Humidity: {humidity}")
-        print(f"Pressure: {pressure}")
+        print(f"Humidity: {humidity} %")
         print(f"Weather Report: {weather_report[0]['description']}")
-        print(f"Wind Speed: {wind_report['speed']}")
-        print(f"Time Zone: {data['timezone']}")
     else:
         # showing the error message
         print("Error in the HTTP request")
